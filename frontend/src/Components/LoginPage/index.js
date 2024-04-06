b9c1a8b5241bce9dee8983111325af2aa044adab
CAYK ANDRADE MAGNANI <1091392313005@fatec-srv.local>
2024-04-05 22:14:02 -0300

    Primeiras mudanças

--- frontend/src/Components/LoginPage/index.js
+++ frontend/src/Components/LoginPage/index.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
+import './Index.css';
 
 function LoginPage() {
   const [activeTab, setActiveTab] = useState("login");
 
   return (
-    <div>
-      <button onClick={() => setActiveTab("login")}>Login</button>
-      <button onClick={() => setActiveTab("register")}>Cadastrar</button>
+    <div className="Tabs">
+      <button className="BotaoTab" id="Esquerda" onClick={() => setActiveTab("login")}>Login</button>
+      <button className="BotaoTab" id="Direita" onClick={() => setActiveTab("register")}>Cadastrar</button>
 
       {activeTab === "login" && <LoginForm />}
       {activeTab === "register" && <RegisterForm />}
@@ -16,6 +17,7 @@ function LoginPage() {
 
 function LoginForm() {
   return (
+    //const DivDireita = document.getElementById("Direita");
     <div>
       <h2>Login</h2>
       {/* Adicione os campos e lógica de login aqui */}
@@ -25,6 +27,7 @@ function LoginForm() {
 
 function RegisterForm() {
   return (
+    //const DivDireita = document.getElementById("Direita");
     <div>
       <h2>Cadastrar Login</h2>
       {/* Adicione os campos e lógica de cadastro aqui */}
