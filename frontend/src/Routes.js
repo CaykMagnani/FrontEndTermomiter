375bf27a3a6ea554fb75ac879acec3e7644a5329
CaykMagnani <142061953+CaykMagnani@users.noreply.github.com>
2024-06-03 20:31:36 -0300

    Update Routes.js

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,4 +1,10 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  Redirect,
+} from "react-router-dom";
 import LoginPage from "./Pages/LoginPage";
 import TemperatureHistory from "./Pages/TemperatureHistory";
 import HumidityHistory from "./Pages/HumidityHistory";
@@ -8,19 +14,19 @@ function RoutesApp() {
   const isLoggedIn = sessionStorage.getItem("isLoggedIn") === "true";
 
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/login"
-            element={isLoggedIn ? <Navigate to="/dashboard" /> : <LoginPage />}
-          />
-          <Route path="/dashboard" element={<Home />} />
-          <Route path="/TemperatureHistory" element={<TemperatureHistory />} />
-          <Route path="/HumidityHistory" element={<HumidityHistory />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/login"
+          element={isLoggedIn ? <Navigate to="/dashboard" /> : <LoginPage />}
+        />
+        {/* Adicione um redirecionamento para a rota do dashboard */}
+        <Route exact path="/" element={<Redirect to="/dashboard" />} />
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/TemperatureHistory" element={<TemperatureHistory />} />
+        <Route path="/HumidityHistory" element={<HumidityHistory />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
